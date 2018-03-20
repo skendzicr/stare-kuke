0f1ab1fe56f47f7e3b2405cebc175c16f7a73c16
Radovan Skendzic <rskendzic@execom.eu>
2018-03-20 22:34:34 +0100

    Changed images. Added latin and cyrilic

---
+++ src/app.js
@@ -0,0 +1,33 @@
+console.log(`MMMMNNMMNNNNNNNNNNNNNNNNNmdhhhhhhysssoo+ossosymNmmNNmmmmNNNNNNNNNNNNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMNM
+MMNNMMMMNNNMNMMNMMNNNNmmhoo+oo++++++/::::::-:/+ohNNmNmNNNNNNNNNNNNNNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMNNMNmhyso++oo++++/::::---------/dmmmNmmmmNNNNNNNNNNNNNNNN STARE MMMMMMMMMMMMMM
+OLD HOOKS MMMMMMNNNNMNNmmhyso++++o+//::::::::--------/dmmmmmmmNNNNNNNNNNNNNNN KUKE MMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNMMNNNMNmhso++++++///:::---------::----hmdmmmNNNNNNNNNNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMNNMMMNNNNNNNMMNdhooossyhhhmmNMmmmmdhyo/--..-----dmmmmmmNNNNNNNNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMNMMMNNNMMMMMMNNNNMMMMMMNNNNNMNmNdmMMMMNds:.....-hmmmmmmmNNNNNNNNNNNNNN MOHIKANCI MMMMMMMMMMMM
+MMMMMMMMMMMNNNMMMMMMMMMMMMMMMMNNNMMMMmNmNMMMMMMMNy+/.--/mmmmmmmmNNNNNNNNNNNNNNMMMMMMMMMM MOJI MMMMMM
+MMMMMMMMMMMMMNNNMMMMNmdhysssssooyhNNMdNmNNMMMMMMMmdNmddhmmmmmmmmmmNmNNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMM
+MMNMMMNNNmNMMMMNMMMNdhyyo+//++/+symMMmMMMMMMMMMMNh+NNMMMMMNmmddmmmmmmmmmmmmNNNNMMMMMMMMMMMMMMMMMMMMM
+MMNMMmmNmNNNNNNMNNNmhyyysoo+/+++oshmmNNNMMMMMNmNd:.-mMMNMMMMNmhsymmmmmmmmmmmNNNNNMMMMMMMMMMMMMMMMMMM
+MMNMNmdhsshmhhmNNNmmhyssyysso/::/+oydmNmmNNNMNms//-.oMMMMNMNhyy/.'/+ymmmmmmmNNNNNNNNNNMMMMMMMMMMNNNN
+MMMMNdd//ohmdyhmNmmddyoosssys+/::::----/+syyso+ss/-..mNdho/odmNh+:.' '+dmmdmNyNNdodNNNNNNNNNNNNmmmdd
+MMMMmsy-/+sddohdmddddyo+oosso+++//:-.'.--os+////-'.odhmhoohmNMNNmyoshNNyydyNNMsyMMMMNMNNdddhhhhhhh
+MMNMMsyooysyhydddhhyysoossso++////:-..'.-'/shmhssosymMMMMMMmNMMMMMMMMMMNmmNmMNNNNMMNmddmNdyyyyyyyyy
+MMMMMNssys++++hdhyyyoossyyyoo+:::/:-..--.-/osyohdmmmNNMMMMMMdMMMMMMMMMMMMMMmdddhhhhhyyyyyNmhyyyyyyyy
+MMMMMMMMmhyyy+hhhyyssosyyyss++/:-:--.-/+sssymshhhhsMMNNMMMMMmMMMMMMMMNmhs/shhyyyyyyyyyyyydNyyyyyyyyy
+MMMMMMMMMMMMMdhhhyysyyyyyyso++//////:/oyhhdNmsdddmhNy///yNMMMNMMMMMMN+--.'/hhyyyyyyyyyyyyyNhyyyyyyyy
+MMMMMMMMMMMMNdhyyyyyyyhhhyys+++++++/:/osydNMNhdNmmMNdo/:/oNMMMMNmhsyh/---.+hhyyyyyyyyyyyyymmyyyyyyyy
+MMMMNMMMMMMMNmhyyyssyhhddhhso+////++///+oshMMNhmNMMMMho/++yMNd+---.'.....shhyyyyyyyyyyyyyhNyyyyyyyy
+MMMMMMMMMMMMNmdhyysshhddddhso+///+o++++/++sdMMMMMMMMMms+++hs+:.'  '-hhyyyyyyyyyyyyyyyNhyyyyyyy
+MMMMMMMMMMMMNNdhyyssyhdmmddhyssssoooossoosssdMMMMMMhmmyssho:.-:::+s+.'-yyyyyyyyyyyyyyyyyNdhyyyyyy
+MMMMMMMMMMMNNNmdysssyyhdmmddhhyysooyhhhssyyyyNMMMMNdNmd/://+sdNNMMNho/-' 'oyyyyyyyyyyyyyyyyNmhyyyyyy
+MMMMMMMMMMMNmmmdhysosyhhdmmmdddhyyhdddddhyhhhhNMmMNNNMMNmNNMMMNMMMNy+/-'  -yyyyyyyyyyyyyyyyNmhyyyyyy
+MMMMMMMMMMNmdddhhysssyyhdmdmmdddddddmmmmmmmmddNMMMMMMMNdhyNMMNNMMMMNh+-'  'yyyyyyyyyyyyyyyyNmhyyyyyy
+MMMMMMMMMMMdhhhhhhyyysyyhddddmdddmmmNNmmNNNMNNMMMMMMmhs/-/dMMNNNMMNho/-'  'syyyyyyyyyyyyyyhNmhyyyyyy
+MMMMMMMMMMMNhhyyyyyyyssyyhhdddhdmmmmmNmmmNNNMMMMMmyNmho/::hNMm+NNNNds/-'  '+yyyyyyyyyyyyyyhMmyyyyyyy
+MMMMMMMMMMMMNyyyyssssssssssyhhdhdmmmmmmNNNNNMMMMNhymNho/::odMm:mhhdhy+-'  ':hyyyyyyyyyyyyydNdyyyyyyy
+MMMMMMMMMMMMMNyssssssssoosssyyhddddmmNNNNNMMMMMMMMmhNdy+//omMy:myhdoo+-'  ':hyyyyyyyyyyyyymNdyyyyyyy
+MMMMMMMMMMMMMMNyssssoooooosyyhhhhhddmmmNNMMMMMMMMMMMMNdysoyNNs/mhdhs/:-':hyyyyyyysyyyyyNNhyyyyyyy
+MMMMMMMMMMMMMMMNhssssoooosssssysyhddddmmNMMMMMMMMMMMMMNmyymNN+odhdhs+/-.'./yyyysyssyyyyyhMmyyyyyyyy
+MMMMMMMMMMMMMMMMMMmdysoossoooooossshdddhNMMMMMMMMMMMMMMMNNMNN/ydhmdyo/-...-+hyyyyssssyyyymNdyyyyyyyy
+IT POLAKO MMMMMMMMMMMMMNdhhyssssssyddmdmMMMMMMMMMMMMMMMMMMMNN/hdhddyo/----//hyysyyssyyyyyNNhyyyyyyyy`)
